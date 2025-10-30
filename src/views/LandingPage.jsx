bc85b773ffe516a39008d1f2129b16dbc0dc0c0c
Christian Esteves <christianesteves@Christians-MacBook-Pro-2.local>
2025-10-30 13:58:42 -0400

    Added calendar to landing page, some style changes

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -3,20 +3,30 @@ import { useNavigate } from "react-router-dom";
 import StickyBanner from "../components/StickyBanner";
 import "../styles/buttons.css";
 import "../styles/footer.css";
+import "../styles/schedule.css"; // styles for VisitSchedule
 
 export default function LandingPage() {
-    const nav = useNavigate();
+  const nav = useNavigate();
 
   return (
     <div className="relative min-h-screen bg-[#F7F5E7] flex flex-col overflow-hidden">
       {/* Banner */}
       <StickyBanner image="/starbucks-banner.jpg" />
 
+      {/* Schedule section */}
+      <div className="schedule-container">
+        <div className="schedule-scrollbox">
+          <VisitSchedule />
+        </div>
+      </div>
+
       {/* Floating button */}
       <main className="participa-container">
-        <button 
-        onClick={() => nav("/Form")}
-        type="button" className="participa-btn">
+        <button
+          onClick={() => nav("/Form")}
+          type="button"
+          className="participa-btn"
+        >
           Participa
         </button>
       </main>
@@ -25,8 +35,12 @@ export default function LandingPage() {
       <footer className="footer">
         <div className="footer-content">
           <p className="footer-text">
-            © {new Date().getFullYear()} Válido del 1 de noviembre al 15 de diciembre de 2025. Participación sin obligación de compra. Participación limitada a mayores de edad y residentes legales de Puerto Rico, con excepción de las personas descritas en las reglas.Reglas completas en www.ganaconstarbucks.com
-          </p>         
+            © {new Date().getFullYear()} Válido del 1 de noviembre al 15 de
+            diciembre de 2025. Participación sin obligación de compra.
+            Participación limitada a mayores de edad y residentes legales de
+            Puerto Rico, con excepción de las personas descritas en las
+            reglas.Reglas completas en www.ganaconstarbucks.com
+          </p>
         </div>
       </footer>
     </div>
